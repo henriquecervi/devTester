67c60a2dd5a3e49e231f0ceba0fa214a92f7c37c
Henrique Cervi <henrique.cervi@terra.com.br>
2021-03-11 21:56:07 -0300

    Criando primeira rota com controller.

---
+++ backend/controllers/contact.controller.js
@@ -0,0 +1,19 @@
+
+module.exports = {
+    list(request, h) {
+        return [
+            {
+                id: 1,
+                name: "Henrique Cervi",
+                number: "11 999999999",
+                description: "Te ajudando à evoluir",
+            },
+            {
+                id: 2,
+                name: "Henrique Cutri",
+                number: "11 999999999",
+                description: "Juntos vamos mais longe",
+            },
+        ]
+    }
+} 
---
+++ backend/routes/contact.routes.js
@@ -0,0 +1,10 @@
+const contactController = require('../controllers/contact.controller')
+
+ module.exports = [
+    {
+        method: 'GET',
+        path: '/contacts',
+        handler: contactController.list        
+        
+    }
+ ]
